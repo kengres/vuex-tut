d387638a5d47d39f8081978f03d928d17c45e637
kenguruka grevisse <kenguruka24@gmail.com>
2017-07-03 17:59:09 +0300

    all comittin

---
+++ src/store.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        registrations: [],
+        users: [
+            {id: 1, name: 'Max', registered: false},
+            {id: 2, name: 'Anna', registered: false},
+            {id: 3, name: 'Chris', registered: false},
+            {id: 4, name: 'Steven', registered: false},
+            {id: 5, name: 'Bakevya', registered: false}
+
+        ]
+    },
+    getters: {
+        unregisteredUsers(state)  {
+            return state.users.filter( user => {
+                return !user.registered;
+            });
+        },
+        registrations(state) {
+            return state.registrations;
+        },
+        totalRegistrations(state) {
+            return state.registrations.length;
+        }
+    }
+});
